09ed8a5177279422a7090a1cb33c66a6fe61c188
Trevor <68612305+treesmashboi@users.noreply.github.com>
2021-01-20 18:10:44 -0500

    Create action.php

---
+++ action.php
@@ -0,0 +1,13 @@
+<?php
+//create function
+function showInput() {
+  echo "<body>";
+  //label the action
+  print "<p>You entered:</p>";
+  //print input from Form @action.html
+  echo $_POST['name'] + ".";
+  echo "</body>";
+};
+//call showInput()
+showInput();
+?>
