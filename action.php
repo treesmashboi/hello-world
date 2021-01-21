f9ecb1d9fa139b712e423891b5edaf4d8324ddeb
Trevor <68612305+treesmashboi@users.noreply.github.com>
2021-01-20 23:04:28 -0500

    Update action.php

--- action.php
+++ action.php
@@ -5,7 +5,7 @@ function showInput() {
   //label the action
   print "<p>You entered:</p>";
   //print input from Form @action.html
-  echo $_POST['name'] + ".";
+  echo $_POST['name'];
   echo "</body>";
 };
 //call showInput()
